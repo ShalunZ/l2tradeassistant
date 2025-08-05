c5913b48f996fd30c5ae6d9a8429f6211f27ca93
ShalunZ <alexanovkirill@gmail.com>
2025-08-05 17:06:59 +0700

    feat(sound): Добавлены асинхронность звуков и Beep-сигналы

--- utils/sound.py
+++ utils/sound.py
@@ -1,17 +1,31 @@
 # utils/sound.py
 import winsound
 
+# 🔊 Системные звуки (асинхронные)
 def play_success_sound():
-    winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS)
+    winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS | winsound.SND_ASYNC)
 
 def play_error_sound():
-    winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
+    winsound.PlaySound("SystemHand", winsound.SND_ALIAS | winsound.SND_ASYNC)
 
 def play_notification_sound():
-    winsound.PlaySound("SystemNotify", winsound.SND_ALIAS)
+    winsound.PlaySound("SystemNotify", winsound.SND_ALIAS | winsound.SND_ASYNC)
 
 def play_question_sound():
-    winsound.PlaySound("SystemQuestion", winsound.SND_ALIAS)
+    winsound.PlaySound("SystemQuestion", winsound.SND_ALIAS | winsound.SND_ASYNC)
 
 def play_loud_sound():
     winsound.MessageBeep(winsound.MB_ICONHAND)
+
+# 🔔 Простые Beep-сигналы
+def beep_short():
+    winsound.Beep(800, 150)  # короткий писк
+
+def beep_long():
+    winsound.Beep(600, 500)  # длинный писк
+
+def beep_high():
+    winsound.Beep(1000, 200)  # высокий писк
+
+def beep_low():
+    winsound.Beep(400, 300)  # низкий писк
